b655148d3465a64f7b14977f7f6289d0d4bf9404
Tamer Wagih <mrtamerwagih@gmail.com>
2020-09-15 22:08:31 +0300

    Adding Authentication

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,16 @@
 export {
-  addIngredient,
-  removeIngredient,
-  initIngredients,
+    addIngredient,
+    removeIngredient,
+    initIngredients
 } from './burgerBuilder';
-export { purchaseBurger, purchaseInit, fetchOrders } from './order';
+export {
+    purchaseBurger,
+    purchaseInit,
+    fetchOrders
+} from './order';
+export {
+    auth,
+    logout,
+    setAuthRedirectPath,
+    authCheckState
+} from './auth';
